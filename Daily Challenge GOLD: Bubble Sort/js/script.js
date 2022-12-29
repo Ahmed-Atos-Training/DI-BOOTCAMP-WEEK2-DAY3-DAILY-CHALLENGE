5276916365893b002a06e52ff06beb010d54c79e
koneahmed <koneahmed100@gmail.com>
2022-12-29 20:03:02 +0000

    first commit

---
+++ Daily Challenge GOLD: Bubble Sort/js/script.js
@@ -0,0 +1,8 @@
+const numbers = [5,0,9,1,7,4,2,6,3,8];
+
+let arrayToString = numbers.toString();
+let arrayToJoin1 = numbers.join("-");
+let arrayToJoin2 = numbers.join(" ");
+let arrayToJoin3 = numbers.join("");
+
+let arrayInOrder = numbers.sort();
